2d913adad88a49f271546dde5ce98d8e1b6dd14d
ankijung88 <ankijung88@gmail.com>
2026-01-27 21:32:30 +0900

    Remove DebugOverlay for production

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css"; // 전역 스타일
-import DebugOverlay from "./components/DebugOverlay";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <DebugOverlay />
       <App />
     </HashRouter>
   </React.StrictMode>
